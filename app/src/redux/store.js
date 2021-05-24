294641a455bb66fb34ca5b9953e7c24423205584
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-05-24 19:29:54 +0200

    #14 minor refactoring + connected redux extension to the window

--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -1,4 +1,9 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// const store = createStore(
+//     reducer, /* preloadedState, */
+//  +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   );
