4fc5d40843b3abf93f495956c97008efd21c4bf2
Kamil Poniatowski <poniatowski.dev@gmail.com>
2021-05-24 19:09:53 +0200

    #14 initial structure for redux

--- app/src/index.js
+++ app/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
