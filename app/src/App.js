0c9081d9231cad43f3d37a290360e1d83161f662
Kamil Poniatowski <poniatowski.dev@gmail.com>
2021-05-29 14:12:59 +0200

    #20 connecting the MainPage module on the main application view

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
-import ExampleServices from './ExampleServices'
+import { Route, Switch } from 'react-router';
+import {NavBar} from './core/components';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
-import {NavBar} from './core/components';
 import {MainContainer} from './core/styledComponents'
 import { UsersView } from './users-section/containers/UsersView';
+import { MainPage } from './modules/MainPage';
 import { DetailsTask, TaskView } from "./modules/tasks";
-import { Route, Switch } from 'react-router';
+import ExampleServices from './ExampleServices'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <MainContainer>
         <Switch>
+          <Route exact path="/" component={MainPage} />
           <Route exact path="/users" component={UsersView} />
           <Route exact path="/tasks/" component={TaskView} />
           <Route exact path="/tasks/:task_id/" component={DetailsTask} />
