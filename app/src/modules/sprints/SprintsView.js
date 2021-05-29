ae57d06a4c45aeb507db62d0c43ac79d67fee445
Bartłomiej Galicki <bartek.galicki@gmail.com>
2021-05-29 21:20:21 +0200

    #23_SprintView-skeleton

--- app/src/modules/sprints/SprintsView.js
+++ app/src/modules/sprints/SprintsView.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import StyledAddButton from './StyledAddButton.style'
+import StyledFinishedContainer from './StyledFinishedContainer.style'
+import StyledOngoingContainer from './StyledOngoingContainer.style'
+import StyledText from './StyledText.style'
 
 
 
@@ -6,7 +10,48 @@ import React from 'react'
 function SprintsView() {
     return (
         <div>
-            
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-2">
+                        <StyledAddButton>
+                            <div className="plusSignWrapper">
+                                <div className='plusSign'></div>
+                            </div>
+                            <p>Add Sprint</p>
+                        </StyledAddButton>
+                    </div>
+                        <div className="col-4">{/* PLACEHOLDER NA TYTUŁ 'KLIKNIĘTEGO' SPRINTU */}</div>
+                        <div className='col-1'></div>
+                        <div className="col-4 text-center"><h1 className='text-secondary'>BACKLOG</h1></div>
+                        <div className='col-1'></div>
+                </div>
+
+                <div className="row mt-2">
+                    <div className="col-2">
+                        <StyledOngoingContainer>
+                            <p>ongoing</p>
+
+                            {/* wstawić funkcję drukującą ongoing sprints */}
+
+                        </StyledOngoingContainer>
+
+                        <StyledFinishedContainer>
+                            <p>finished</p>
+
+                            {/* wstawić funkcję drukującą finished sprints */}
+
+                        </StyledFinishedContainer>
+
+                    </div>
+                    <div className="col-4 border border-darken-1">
+                        
+                    </div>
+                    <div className='col-1'></div>
+                    <div className="col-4 border border-darken-1">
+                    </div>
+                    <div className='col-1'></div>
+                </div>
+            </div>
         </div>
 
 
