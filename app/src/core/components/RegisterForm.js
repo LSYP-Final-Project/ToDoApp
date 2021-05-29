254299af92085e35110d7251d20416fd9647197a
Jacek Krużycki <kruzyk@gmail.com>
2021-05-30 00:42:12 +0200

    repairs labels and adds onReset and onSubmit

--- app/src/core/components/RegisterForm.js
+++ app/src/core/components/RegisterForm.js
@@ -1,26 +1,36 @@
 import React from 'react'
-// import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
 function RegisterForm() {
+
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = (data, e) => {
+        // e.target.reset(); // reset after form submit
+        debugger;
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(data));
+    };
+
     return (
         <div className="container d-flex justify-content-center">
             <div className="card m-3 w-75">
                 <h5 className="card-header">Create account</h5>
                 <div className="card-body">
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)} onReset={reset}>
                         <div className="row mb-3">
                             <div className="col">
-                                <label className="form-label" for="inputEmail">Email</label>
+                                <label className="form-label" htmlFor="inputEmail">Email</label>
                                 <input className="form-control" name="email" type="text" id="inputEmail" required />
+                                {/* {errors.email && <p>This is required</p>} */}
                             </div>
                         </div>
                         <div className="row mb-3">
                             <div className="col">
-                                <label className="form-label" for="inputPassword">Password</label>
+                                <label className="form-label" htmlFor="inputPassword">Password</label>
                                 <input className="form-control" name="password" type="password" id="inputPassword" required />
                             </div>
                             <div className="col">
-                                <label className="form-label" for="inputConfirmPassword">Confirm Password</label>
+                                <label className="form-label" htmlFor="inputConfirmPassword">Confirm Password</label>
                                 <input className="form-control" name="confirmPassword" type="password" id="inputConfirmPassword" required />
                             </div>
                         </div>
