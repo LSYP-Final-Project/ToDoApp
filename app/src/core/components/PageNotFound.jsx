5fef5eb29fe784981d7a671e947530da966f92ea
Kamil Poniatowski <poniatowski.dev@gmail.com>
2021-05-29 15:48:30 +0200

    #20 adding material_icons, new styles for PageNotFound and svg for 404

--- app/src/core/components/PageNotFound.jsx
+++ app/src/core/components/PageNotFound.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { SvgPageNotFound } from '../svg'
 
 const PageNotFound = () => (
-    <div>
-        <h1>404 - Not Found!</h1>
-        <Link to="/">
-            Go Home
-    </Link>
+    <div className="container">
+        <div className="row">
+            <div className="d-flex justify-content-center">
+                <Link to="/" className="btn btn-primary btn-lg" aria-label="Back to Home Page">
+                    <div className="d-flex align-items-center">
+                        <span class="material-icons me-2">home</span>
+                        Take Me Home
+                    </div>
+                </Link>
+            </div>
+        </div>
+        <SvgPageNotFound />
     </div>
 );
 
