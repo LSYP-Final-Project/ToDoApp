ec8af927f3ba2b11b51b8e7ca47e5aeb72ca87e0
Jacek Krużycki <kruzyk@gmail.com>
2021-05-30 22:02:16 +0200

    adds Redux action and reducer for ADD_USER

--- app/src/redux/actionTypes.js
+++ app/src/redux/actionTypes.js
@@ -2,3 +2,4 @@ export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 export const SET_FILTER = "SET_FILTER";
 export const GET_USERS = "GET_USERS";
+export const ADD_USER = "ADD_USER";
