2b09f7b494f5ec0c1464b3918980d096b674e295
Kamil Poniatowski <poniatowski.dev@gmail.com>
2021-05-30 14:50:19 +0200

    #20 add max-width for TaskList

--- app/src/core/components/index.js
+++ app/src/core/components/index.js
@@ -1,3 +1,3 @@
-export {default as NavBar} from './NavBar'
-export {default as PageNotFound} from './PageNotFound'
-export {default as TaskCard} from './TaskCard'
+export { default as NavBar } from './NavBar'
+export { default as PageNotFound } from './PageNotFound'
+export { default as TaskCard } from './TaskCard'
--- app/src/modules/MainPage/components/TaskList.jsx
+++ app/src/modules/MainPage/components/TaskList.jsx
@@ -3,7 +3,7 @@ import { TaskCard } from '../../../core/components'
 
 export default function TasksList({ title, tasks }) {
     return (
-        <article className="card shadow-sm border-light rounded-0">
+        <article className="main-page__task-list card shadow-sm border-light rounded-0">
             <div className="card-body">
                 <h3 className="card-title">{title}</h3>
                 {tasks.length && tasks.map(t => <TaskCard key={t.id} task={t} />)}
