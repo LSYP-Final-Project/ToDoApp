17a8bb591c1902cb9a4f9b16436d4f902ac9ba03
Patrycja <paczkowskapat@gmail.com>
2021-06-04 20:31:54 +0200

    change functional component to class

--- app/src/modules/tasks/containers/TaskView.jsx
+++ app/src/modules/tasks/containers/TaskView.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import TaskList from "../components/TaskList";
 import SearchTasks from "../components/SearchTasks";
 
-const TaskView = () => {
-	return (
-		<div className="container mt-3">
-			<p className="display-6">Hi! You will find your tasks below:</p>
-			<SearchTasks />
-			<TaskList />
-		</div>
-	);
-};
+class TaskView extends React.Component {
+	reneer() {
+		return (
+			<div className="container mt-3">
+				<p className="display-6">Hi! You will find your tasks below:</p>
+				<SearchTasks />
+				<TaskList />
+			</div>
+		);
+	}
+}
 
 export default TaskView;
