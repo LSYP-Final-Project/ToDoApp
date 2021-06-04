9a41f1bf86b66d09c54dd73a7e696d4bf279aff1
Jacek Krużycki <kruzyk@gmail.com>
2021-06-04 21:46:21 +0200

    cleans code from comments

--- app/src/modules/login/containers/RegisterView.jsx
+++ app/src/modules/login/containers/RegisterView.jsx
@@ -3,9 +3,6 @@ import StyledNotice from 'Core/styledComponents/Notice'
 import RegisterForm from '../components/RegisterForm'
 
 export default function RegisterView() {
-    // const userTasks = useSelector(getUserTasks);
-    // const latestTasks = useSelector(getLatestTasks);
-    // const latestComments = useSelector(getLatestComments);
 
     return (
         <main className="d-flex flex-column flex-lg-row px-2">
@@ -13,7 +10,7 @@ export default function RegisterView() {
                 <RegisterForm />
             </div>
             <div className="col-lg-6 m-3">
-                <StyledNotice title='Why register?' content='You have to register to view your tasks' />
+                <StyledNotice title='Why register?' content='A registered user can manage his tasks and track tasks and progress' />
             </div>
         </main>
     )
