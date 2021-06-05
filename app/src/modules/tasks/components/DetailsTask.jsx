0ec0ee591cd778f6624978771cd5ee7f7869be54
Patrycja <paczkowskapat@gmail.com>
2021-06-05 18:08:24 +0200

    #11 preparing to launching storybook

--- app/src/modules/tasks/components/DetailsTask.jsx
+++ app/src/modules/tasks/components/DetailsTask.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistoryRouter } from 'Core/hooks/useHistoryRouter'
-import { convertMsToLocalDate } from 'Core/helpers/functions/timeAndDate'
+import { useHistoryRouter } from "Core/hooks/useHistoryRouter";
+import { convertMsToLocalDate } from "Core/helpers/functions/timeAndDate";
 import { getTaskComments, fetchComments, addComment } from "Redux/reducers/comments";
 import { getTodo } from "Redux/reducers/todos";
 import CommentsCard from "./CommentsCard";
-import { CommentsService } from 'Services'
+import { CommentsService } from "Services";
 
 const DetailsTask = () => {
 	const [addingComment, setAddingComment] = useState(false);
@@ -14,10 +14,6 @@ const DetailsTask = () => {
 	const { goBack } = useHistoryRouter();
 	const comments = useSelector(getTaskComments);
 	const task = useSelector(getTodo);
-
-	useEffect(() => {
-		dispatch(fetchComments());
-	});
 
 	const addNewComment = () => {
 		const date = new Date().getTime();
@@ -37,7 +33,11 @@ const DetailsTask = () => {
 		setNewTaskTitle("");
 	};
 
-	if (!task) return
+	useEffect(() => {
+		dispatch(fetchComments());
+	});
+
+	if (!task) return;
 	return (
 		<div className="container mt-3">
 			<p className="display-6">Details:</p>
@@ -48,7 +48,9 @@ const DetailsTask = () => {
 					<h6>Created by: {task.userId}</h6>
 					<h6>Created at: {convertMsToLocalDate(task.createdAt)}</h6>
 					<h6>Status: {task.status}</h6>
-					<button onClick={goBack} className="btn btn-primary mt-4">Go back</button>
+					<button onClick={goBack} className="btn btn-primary mt-4">
+						Go back
+					</button>
 				</div>
 				<div className="container col-sm">
 					<div className="card mb-3">
@@ -64,7 +66,7 @@ const DetailsTask = () => {
 								Add Comment
 							</button>
 						</div>
-						{addingComment &&
+						{addingComment && (
 							<div className="input-group mb-3 p-3">
 								<input
 									type="text"
@@ -79,7 +81,7 @@ const DetailsTask = () => {
 									</button>
 								</div>
 							</div>
-						}
+						)}
 					</div>
 				</div>
 			</div>
