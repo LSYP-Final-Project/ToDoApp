d5b7bb4b768fcf2ba5a2702a7f7ac39fbb5b2c5c
Patrycja <paczkowskapat@gmail.com>
2021-06-06 17:10:51 +0200

    #11 using function from helpers in CommentsCard

--- app/src/modules/tasks/components/CommentsCard.jsx
+++ app/src/modules/tasks/components/CommentsCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {convertMsToLocalDate} from 'Core/helpers/functions/timeAndDate';
 
 const CommentsCard = ({ comment }) => {
-   const date = new Date(comment.createdAt);
+   // const date = new Date(comment.createdAt);
 
 	return (
 		<div className="list-group-item ">
-			{comment.content} <span className="fst-italic text-secondary"> - wrote by {comment.userId} at {date.toLocaleDateString()}</span>
+			{comment.content} <span className="fst-italic text-secondary"> - wrote by {comment.userId} at {convertMsToLocalDate(comment.createdAt)}</span>
 		</div>
 	);
 };
