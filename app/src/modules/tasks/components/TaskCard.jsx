dce40419abb286c8cd08546cc5e51ff690e3ead2
Patrycja <paczkowskapat@gmail.com>
2021-06-06 17:04:25 +0200

    #11 adding styledcomponents to TaskCard

--- app/src/modules/tasks/components/TaskCard.jsx
+++ app/src/modules/tasks/components/TaskCard.jsx
@@ -1,22 +1,28 @@
 import React, { useCallback } from "react";
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { selectTaskId } from 'Redux/reducers/todos';
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectTaskId } from "Redux/reducers/todos";
+import styled from "styled-components";
+
+const SpanStyle = styled.span`
+	font-style: italic;
+`;
 
 const TaskCard = ({ task }) => {
 	const dispatch = useDispatch();
 
 	const onTaskCardClick = useCallback(() => {
 		dispatch(selectTaskId(task.id));
-	},[]);
+	}, []);
 
 	return (
-		<Link to={`/tasks/${task.id}`}
+		<Link
+			to={`/tasks/${task.id}`}
 			className={`list-group-item list-group-item-action`}
 			onClick={onTaskCardClick}
 		>
-			{task.title} (created by {task.userId}) - <span className="fst-italic">{task.status}</span>
-
+			{task.title} (created by {task.userId}) -{" "}
+			<SpanStyle className="fst-italic">{task.status}</SpanStyle>
 		</Link>
 	);
 };
